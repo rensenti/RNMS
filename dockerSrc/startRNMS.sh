1672c1d3ba95c7331942ce9344316885c85b2521
rensenti <rene@njahaha.net>
2020-06-26 21:03:38 +0200

    RNMS_IP varijabla

--- dockerSrc/startRNMS.sh
+++ dockerSrc/startRNMS.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 # za bind mount
 # ako je na hostu taj dir prazan, popuni sadrzaj tog dira sa Src
 # ako nije prazan onda je RNMS vec bio pokrenut i vec ima strukturu (i podatke)
 if [ $(find $RNMS_PREFIX -type f | wc -l) -lt 2 ]; then cp -Rp /var/tmp/Src/* $RNMS_PREFIX/; fi
-sucelje=$(ip route | grep default | grep -Po 'dev\s\w+');
-RNMS_IP=$(ip route | grep -P "$sucelje.*src" | head -1 | awk -F "src " '{print $2}' | awk '{print $1}')
-export RNMS_IP
+. $RNMS_PREFIX/pomagalice
 
 service cron start
 rm -f /usr/local/apache2/logs/httpd.pid
@@ -18,7 +16,7 @@ cat << EOF
 #*************************************************************************
 #
 #   URL ZA PRISTUP RNMS WEB SUCELJU:
-#           - http://$RNMS_IP
+#           - http://$(hostIP)
 #
 #*************************************************************************
 EOF
