96fb07dd862fd408372a1e3dd8732f8da622349c
rensenti <rene@njahaha.net>
2020-06-26 21:45:42 +0200

    popravak ENV varijabli via pomagalica

--- dockerSrc/startRNMS.sh
+++ dockerSrc/startRNMS.sh
@@ -3,7 +3,10 @@ set -e
 # za bind mount
 # ako je na hostu taj dir prazan, popuni sadrzaj tog dira sa Src
 # ako nije prazan onda je RNMS vec bio pokrenut i vec ima strukturu (i podatke)
-if [ $(find $RNMS_PREFIX -type f | wc -l) -lt 2 ]; then cp -Rp /var/tmp/Src/* $RNMS_PREFIX/; fi
+if [ $(find $RNMS_PREFIX -type f | wc -l) -lt 2 ]; then
+	cp -Rp /var/tmp/Src/* $RNMS_PREFIX/;
+	export -p >> $RNMS_PREFIX/bin/pomagalice
+fi
 . $RNMS_PREFIX/bin/pomagalice
 
 service cron start
