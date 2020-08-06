c89364cbe30a5c682a0d529a49429cd9f229ec2a
rensenti <rene@njahaha.net>
2020-08-06 21:10:28 +0200

    duže čekaj na template, 600 sekundi timeout

--- dockerSrc/bin/controlNetflow.sh
+++ dockerSrc/bin/controlNetflow.sh
@@ -12,7 +12,7 @@ fi
 # kreiraj argumente nfcapd-u za netflow direktorij po ID-u
 # npr.
 # -n prvi,10.16.68.250,/RNMS/netflow/prvi
-/usr/bin/nfcapd -P /var/run/nfcapd.pid -D -T all -p 2055 \
+/usr/bin/nfcapd -P /var/run/nfcapd.pid -D -t 600 -T all -p 2055 \
 $(for line in $(su - postgres -c "psql rnms -c \"copy (select uredjaji.id,uredjaji.ip from uredjaji where netflow='da') to STDOUT WITH CSV HEADER;\"" | tail -n +2); do nodeId=$(echo $line | awk -F "," '{print $1}'); nodeIp=$(echo $line | awk -F "," '{print $2}'); echo " -n $nodeId,$nodeIp,/RNMS/netflow/$nodeId ";  done;) 
 
 if [ $? -eq 0  ]; then
