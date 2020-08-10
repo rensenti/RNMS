8b5b12a11d1049dddabb6ab891a9b38eaa7742f4
rensenti <rene@njahaha.net>
2020-08-10 23:26:03 +0200

    obrisi i netflow i routing

--- dockerSrc/bin/brisi.sh
+++ dockerSrc/bin/brisi.sh
@@ -4,4 +4,6 @@
 su - postgres -c "psql rnms -c \"delete from kartice;\""; su - postgres -c " psql rnms -c \"delete from sucelja;\""; su - postgres -c "psql rnms -c \"delete from uredjaji;\""
 rm -f $RNMS_PREFIX/web_aplikacija/slike/perGrafovi/*
 rm -rf $RNMS_PREFIX/rrdb/* 2>/dev/null
+rm -rf $RNMS_PREFIX/netflow/* 2>/dev/null
+rm -rf $RNMS_PREFIX/routing/* 2>/dev/null
 crontab -r
