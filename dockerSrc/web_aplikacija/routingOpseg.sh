0c960abe52bd17182024a044be84185c60b8d3dd
rensenti <rene@njahaha.net>
2020-08-10 23:06:06 +0200

    popravak routing

--- dockerSrc/web_aplikacija/routingOpseg.sh
+++ dockerSrc/web_aplikacija/routingOpseg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . pomagalice
-uredjaji=$(upitBaza "select uredjaji.id,uredjaji.ip,uredjaji.hostname,uredjaji.snmp,uredjaji.netflow from uredjaji ORDER BY uredjaji.id")
+uredjaji=$(upitBaza "select uredjaji.id,uredjaji.ip,uredjaji.hostname,uredjaji.snmp,uredjaji.routing from uredjaji ORDER BY uredjaji.id")
 IFS=$'\n'
 echo "Content-Type: text/html"
 echo
@@ -11,10 +11,10 @@ echo " <title>RNMS uređaji</title>"
 echo " <link rel="stylesheet" href="style.css">"
 echo "</head>"
 echo "<body>"
-echo "<p class="small"><a href="index.sh">< RNMS početna</a></p><h1>NETFLOW KONFIGURACIJA</h1>"
+echo "<p class="small"><a href="index.sh">< RNMS početna</a></p><h1>KONFIGURACIJA NADZORA USMJERNIČKIH TABLICA</h1>"
 echo "<section id="forma">"
 echo "<div class="container">"
-echo "<form class="forma" action="configureNetflow.sh" method="post">"
+echo "<form class="forma" action="configureRouting.sh" method="post">"
 
  echo "<table>" 
  echo "<thead>"
@@ -23,7 +23,7 @@ echo "<form class="forma" action="configureNetflow.sh" method="post">"
  echo "  <th>Node IP</th>"
  echo "  <th>Hostname</th>"
  echo "  <th>SNMP</th>"
- echo "  <th>NetFlow</th>"
+ echo "  <th>Nadzor usmjerničkih tablica</th>"
  echo " </tr>"
  echo "</thead>"
 
@@ -32,17 +32,16 @@ for line in $uredjaji; do
 	nodeIP=$(echo $line | awk -F , '{print $2}')
 	nodeHostname=$(echo $line | awk -F , '{print $3}')
 	SNMP=$(echo $line | awk -F , '{print $4}')
-	netflow=$(echo $line | awk -F , '{print $5}')
+	routing=$(echo $line | awk -F , '{print $5}')
 	echo "<tr>";
-	#echo "<td><a href="reconfigure.sh%20${nodeId}" class="table">$nodeIP</a></td>";
 	echo "<td>$nodeId</td>"
 	echo "<td>$nodeIP</td>";
 	echo "<td>$nodeHostname</td>";
 	echo "<td>$SNMP</td>";
-	if [ "$netflow" == "da" ]; then
-		echo "<td> <input type="checkbox" id="netflow" name="node" value="${nodeId}_${nodeIP}_${netflow} checked"></td>";
+	if [ "$routing" == "da" ]; then
+		echo "<td> <input type="checkbox" id="routing" name="node" value="${nodeId}_${nodeIP}_${routing} checked"></td>";
 	else
-		echo "<td> <input type="checkbox" id="netflow" name="node" value="${nodeId}_${nodeIP}_${netflow}"></td>";
+		echo "<td> <input type="checkbox" id="routing" name="node" value="${nodeId}_${nodeIP}_${routing}"></td>";
 	fi
 	echo "</tr>";
 done
