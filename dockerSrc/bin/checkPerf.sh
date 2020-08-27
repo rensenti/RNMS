5548c3498867a02c68183d77a4918ec9ac99d131
Debeuc Rene <Rene.Debeuc@snt.hr>
2020-08-28 00:25:45 +0200

    popravak logike paketi vs byte + novi graf + util

--- dockerSrc/bin/checkPerf.sh
+++ dockerSrc/bin/checkPerf.sh
@@ -13,15 +13,18 @@ azurirajBazu () {
         mkdir -p $baza && rmdir $baza # :)
         rrdtool create $baza \
             --step $pollingSek \
-                DS:savDolazni:COUNTER:$heartbeat:U:U \
-                DS:savOdlazni:COUNTER:$heartbeat:U:U \
+                DS:ifHCInOctets:COUNTER:$heartbeat:U:U \
+                DS:ifHCOutOctets:COUNTER:$heartbeat:U:U \
+                DS:sviDolazniPaketi:COUNTER:$heartbeat:U:U \
+                DS:sviOdlazniPaketi:COUNTER:$heartbeat:U:U \
                 DS:ifInErrors:COUNTER:$heartbeat:U:U \
                 DS:ifOutErrors:COUNTER:$heartbeat:U:U \
                 DS:ifInDiscards:COUNTER:$heartbeat:U:U \
                 DS:ifOutDiscards:COUNTER:$heartbeat:U:U \
+                DS:ifSpeed:GAUGE:$heartbeat:U:U \
                 RRA:AVERAGE:0.5:1:300
     fi
-    rrdtool update $baza $vrijeme:$savDolazni:$savOdlazni:$ifInErrors:$ifOutErrors:$ifInDiscards:$ifOutDiscards
+    rrdtool update $baza $vrijeme:$ifHCInOctets:$ifHCOutOctets:$sviDolazniPaketi:$sviOdlazniPaketi:$ifInErrors:$ifOutErrors:$ifInDiscards:$ifOutDiscards:$ifSpeed
     echo " -sucelje ${ifName} (index $ifIndex):"
     echo "   -kreirana ili azurirana RRDTOOL baza podataka: $baza"
 }
@@ -36,7 +39,7 @@ narisiGraf () {
             $graf \
             --start -${grafUnazad} \
             -S $granulacija \
-            --width 400 \
+            --width 450 \
             --height 160 \
             -t "${ip}:${ifName}" \
             -z -c "BACK#616066" \
@@ -53,43 +56,101 @@ narisiGraf () {
             -c "BACK#000000"\
             -l 0 \
             -a PNG \
-            -v "bps" \
-            DEF:savDolazni=$baza:savDolazni:AVERAGE \
-            DEF:savOdlazni=$baza:savOdlazni:AVERAGE \
+            -v "bajtovi u sekundi" \
+            DEF:ifHCInOctets=$baza:ifHCInOctets:AVERAGE \
+            DEF:ifHCOutOctets=$baza:ifHCOutOctets:AVERAGE \
+            DEF:ifSpeed=$baza:ifSpeed:MAX \
+            CDEF:bitsIn=ifHCInOctets,8,* \
+            CDEF:bitsOut=ifHCOutOctets,8,* \
+            CDEF:postotakIn=bitsIn,ifSpeed,/,100,* \
+            CDEF:postotakOut=bitsOut,ifSpeed,/,100,* \
+            LINE1:ifHCInOctets#FFFFFF:"Dolazni promet" \
+            GPRINT:ifHCInOctets:LAST:"SAD\\: %5.2lf %s" \
+            GPRINT:postotakIn:LAST:"Iskoristenost veze\\: %3.4lf " \
+            LINE2:ifHCOutOctets#00FF00:"Odlazni promet" \
+            GPRINT:ifHCOutOctets:LAST:"SAD\\: %5.2lf %s" \
+            GPRINT:postotakOut:LAST:"Iskoristenost veze\\: %3.4lf " > /dev/null 2>&1
+    echo "   -kreiran ili azuriran RRDTOOL graf: $URL/${ip}_${ifNameURLFriendly}.png"
+
+    graf=$graphDir/${ip}_${ifNameURLFriendly}_paketi.png
+    rrdtool graph \
+            $graf \
+            --start -${grafUnazad} \
+            -S $granulacija \
+            --width 450 \
+            --height 160 \
+            -t "${ip}:${ifName}" \
+            -z -c "BACK#616066" \
+            -c "SHADEA#FFFFFF" \
+            -c "SHADEB#FFFFFF" \
+            -c "MGRID#AAAAAA" \
+            -c "GRID#CCCCCC" \
+            -c "ARROW#333333" \
+            -c "FONT#333333" \
+            -c "AXIS#333333" \
+            -c "FRAME#333333" \
+            -c "CANVAS#000000" \
+            -c "FONT#FFFFFF" \
+            -c "BACK#000000"\
+            -l 0 \
+            -a PNG \
+            -v "paketi po sekundi" \
+            DEF:sviDolazniPaketi=$baza:sviDolazniPaketi:AVERAGE \
+            DEF:sviOdlazniPaketi=$baza:sviOdlazniPaketi:AVERAGE \
             DEF:ifInErrors=$baza:ifInErrors:AVERAGE \
             DEF:ifOutErrors=$baza:ifOutErrors:AVERAGE \
             DEF:ifInDiscards=$baza:ifInDiscards:AVERAGE \
             DEF:ifOutDiscards=$baza:ifOutDiscards:AVERAGE \
-            LINE1:savDolazni#FFFFFF:"Dolazni promet" \
-            LINE2:savOdlazni#00FF00:"Odlazni promet" \
-            LINE3:ifInErrors#E5003D:"Errors in" \
-            LINE4:ifOutErrors#E50008:"Erorrs out" \
-            LINE5:ifInDiscards#E54A00:"Discards in" \
-            LINE6:ifOutDiscards#E59C00:"Discards out" > /dev/null 2>&1
-    echo "   -kreiran ili azuriran RRDTOOL graf: $URL/${ip}_${ifNameURLFriendly}.png"
+            LINE1:sviDolazniPaketi#FFFFFF:"Dolazni paketi" \
+            GPRINT:sviDolazniPaketi:LAST:"SAD\\: %6.1lf" \
+            GPRINT:sviDolazniPaketi:MIN:"MIN\\: %6.1lf" \
+            GPRINT:sviDolazniPaketi:MAX:"MAKS\\: %6.1lf" \
+            LINE2:sviOdlazniPaketi#00FF00:"Odlazni paketi" \
+            GPRINT:sviOdlazniPaketi:LAST:"SAD\\: %6.1lf" \
+            GPRINT:sviOdlazniPaketi:MIN:"MIN\\: %6.1lf" \
+            GPRINT:sviOdlazniPaketi:MAX:"MAKS\\: %6.1lf" \
+            LINE3:ifInErrors#E5003D:"Errors dolazno" \
+            GPRINT:ifInErrors:LAST:"SAD\\: %6.1lf" \
+            GPRINT:ifInErrors:MIN:"MIN\\: %6.1lf" \
+            GPRINT:ifInErrors:MAX:"MAKS\\: %6.1lf" \
+            LINE4:ifOutErrors#E50008:"Erorrs odlazno" \
+            GPRINT:ifOutErrors:LAST:"SAD\\: %6.1lf" \
+            GPRINT:ifOutErrors:MIN:"MIN\\: %6.1lf" \
+            GPRINT:ifOutErrors:MAX:"MAKS\\: %6.1lf" \
+            LINE5:ifInDiscards#E54A00:"Discards dolazno" \
+            GPRINT:ifInDiscards:LAST:"SAD\\: %6.1lf" \
+            GPRINT:ifInDiscards:MIN:"MIN\\: %6.1lf" \
+            GPRINT:ifInDiscards:MAX:"MAKS\\: %6.1lf" \
+            LINE6:ifOutDiscards#E59C00:"Discards odlazno" \
+            GPRINT:ifOutDiscards:LAST:"SAD\\: %6.1lf" \
+            GPRINT:ifOutDiscards:MIN:"MIN\\: %6.1lf" \
+            GPRINT:ifOutDiscards:MAX:"MAKS\\: %6.1lf" > /dev/null 2>&1
+    echo "   -kreiran ili azuriran RRDTOOL graf: $URL/${ip}_${ifNameURLFriendly}_paketi.png"
+
+
  }
 
 preuzmiPerfPodatke () {
     vrijeme=$(date +%s)
-    # ifInOctets=$(snmpget -v 2c -c $community $ip 1.3.6.1.2.1.2.2.1.10.${ifIndex} | awk -F "Counter32: " '{print $2}')
-    # ifOutOctets=$(snmpget -v 2c -c $community $ip 1.3.6.1.2.1.2.2.1.16.${ifIndex} | awk -F "Counter32: " '{print $2}')
     # DOLAZNI
     ifHCInOctets=$(snmpget -m all -v 2c -c $community $ip ifHCInOctets.${ifIndex} | awk -F "Counter64: " '{print $2}')
     ifHCInUcastPkts=$(snmpget -m all -v 2c -c $community $ip ifHCInUcastPkts.${ifIndex} | awk -F "Counter64: " '{print $2}')
     ifHCInMulticastPkts=$(snmpget -m all -v 2c -c $community $ip ifHCInMulticastPkts.${ifIndex} | awk -F "Counter64: " '{print $2}')
     ifHCInBroadcastPkts=$(snmpget -m all -v 2c -c $community $ip ifHCInBroadcastPkts.${ifIndex} | awk -F "Counter64: " '{print $2}')
-    savDolazni=$(( $ifHCInOctets + $ifHCInUcastPkts + $ifHCInMulticastPkts + $ifHCInBroadcastPkts ))
+    sviDolazniPaketi=$(( $ifHCInUcastPkts + $ifHCInMulticastPkts + $ifHCInBroadcastPkts ))
     # ODLAZNI
     ifHCOutOctets=$(snmpget -m all -v 2c -c $community $ip ifHCOutOctets.${ifIndex} | awk -F "Counter64: " '{print $2}')
     ifHCOutUcastPkts=$(snmpget -m all -v 2c -c $community $ip ifHCOutUcastPkts.${ifIndex} | awk -F "Counter64: " '{print $2}')
     ifHCOutMulticastPkts=$(snmpget -m all -v 2c -c $community $ip ifHCOutMulticastPkts.${ifIndex} | awk -F "Counter64: " '{print $2}')
     ifHCOutBroadcastPkts=$(snmpget -m all -v 2c -c $community $ip ifHCOutBroadcastPkts.${ifIndex} | awk -F "Counter64: " '{print $2}')
-    savOdlazni=$(( $ifHCOutOctets + $ifHCOutUcastPkts + $ifHCOutMulticastPkts + $ifHCOutBroadcastPkts ))
+    sviOdlazniPaketi=$(( $ifHCOutUcastPkts + $ifHCOutMulticastPkts + $ifHCOutBroadcastPkts ))
     # GRIJEŠKE
     ifInErrors=$(snmpget -v 2c -c $community $ip 1.3.6.1.2.1.2.2.1.14.${ifIndex} | awk -F "Counter32: " '{print $2}')
     ifOutErrors=$(snmpget -v 2c -c $community $ip 1.3.6.1.2.1.2.2.1.18.${ifIndex} | awk -F "Counter32: " '{print $2}')
     ifInDiscards=$(snmpget -v 2c -c $community $ip 1.3.6.1.2.1.2.2.1.13.${ifIndex} | awk -F "Counter32: " '{print $2}')
     ifOutDiscards=$(snmpget -v 2c -c $community $ip 1.3.6.1.2.1.2.2.1.19.${ifIndex} | awk -F "Counter32: " '{print $2}')
+    # OSTALO
+    ifSpeed=$(snmpget -m all -v2c -c $community $ip ifSpeed.${ifIndex} | awk -F "Gauge32: " '{print $2}' | head -1)
 }
 
 rnmsOpseg () {
